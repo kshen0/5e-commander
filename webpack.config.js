e5b663cdebb1e94baa1ccec02018ba86141c3ad4
Kevin Shen <kevin.shen@clever.com>
2017-01-26 11:03:30 -0800

    copy from @clever/frontend-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, 'demo/index.js'),
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    devtool: 'eval-source-map',
+    loaders: [
+      {
+        test: /\.(eot|woff|svg|ttf|png)$/,
+        loader: 'url',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
+
